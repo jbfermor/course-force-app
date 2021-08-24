1c24cfec89410a51a6b209ab325b35c3708bf4c9
jbfermor <jbfermor@hotmail.com>
2021-08-24 19:46:22 +0200

    add references courses/user

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create!(email: 'admin@ejemplo.com',
+   password: 'password',
+   password_confirmation: 'password')
+
 30.times do
   Course.create! (
     [
       {
         title: Faker::Educator.course_name,
-        description: Faker::TvShows::GameOfThrones.quote
+        description: Faker::TvShows::GameOfThrones.quote, 
+        user_id: User.first.id
       }
     ]
   )
